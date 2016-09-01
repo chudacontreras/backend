aa9eafc3091c112d1be93118fb7c6e6f71199f31
chuda <chuda@Sonny>
2016-09-01 15:19:37 -0400

    agregadas las migraciones de cursos y cursos para empresas

--- app/EnterpriseCourse.php
+++ app/EnterpriseCourse.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class news extends Model
+class EnterpriseCourse extends Model
 {
     //
 }
---
+++ database/migrations/2016_09_01_211238_create_course_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCourseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('duration');
+            $table->string('picture_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('course');
+    }
+}
